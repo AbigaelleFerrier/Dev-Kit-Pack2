be3f511c7141fd000baa191059b339152784d47c
Cirill Ferrier <34445590+FerrierCirill@users.noreply.github.com>
2019-11-06 19:14:49 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Dev-Kit-Pack2
+# Dev-Kit-Pack
+My package for not dev from scratch
+
+last update [21/10/2019]
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    include 'assets/php/controler/CONTROLER.php' ;
+    echo HEAD::headerHTML('Accueil');
+?>
+    <body>
+        <?php include 'assets/php/common/menu.php' ?>
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>ACCUEIL</h1>
+                </div>
+            </div>
+            
+        <?php include 'assets/php/common/footer.php' ?>
+     
+    </body>
+</html>
